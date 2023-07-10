ee677210152f3b633bdd5c7bc2e51f1cb4fae529
James Cox <james.cox@harness.io>
2023-07-10 11:16:26 +0100

    FFM-8618 Add SetFlags & SetSegments methods
    **What**
    
    - Adds a SetFlags and SetSegment methods to the repository
    
    **Why**
    
    The Proxy passes its cache to the SDK and lets it manage populating and
    updating the cache with flags & segments. The current SetFlag and
    SetSegment methods work for storing individual key/value pairs of flags
    and segments. But we need to be able to store all of the flags/segments
    for a given environment at a single key.
    
    **Testing**
    
    I updated my local proxy to use this version of the sdk and did some
    manual testing and its populating & updating the cache properly when I
    start the proxy up and then toggle/edit a flag/segment

--- dto/key.go
+++ dto/key.go
@@ -7,14 +7,24 @@ type Key struct {
 }
 
 const (
+	// KeyFeatures ...
+	KeyFeatures = "flags"
+
 	// KeyFeature identifies flag messages from ff server or stream
 	KeyFeature = "flag"
+
 	// KeySegment identifies segment messages from ff server or stream
 	KeySegment = "target-segment"
+
+	// KeySegment identifies segment messages from ff server or stream
+	KeySegments = "target-segments"
+
 	// SsePatchEvent identifies a patch event from the SSE stream
 	SsePatchEvent = "patch"
+
 	// SseDeleteEvent identifies a delete event from the SSE stream
 	SseDeleteEvent = "delete"
+
 	// SseCreateEvent identifies a create event from the SSE stream
 	SseCreateEvent = "create"
 )
