eb35ce751b2fba9c7c136888c2968c8d5273ebf0
Erdi Rowlands <erdirowlands@gmail.com>
2024-04-16 19:54:50 +0100

    FFM-11212 New test for listener

--- analyticsservice/analytics_test.go
+++ analyticsservice/analytics_test.go
@@ -9,57 +9,6 @@ import (
 	"github.com/harness/ff-golang-server-sdk/rest"
 )
 
-func Test_convertInterfaceToString(t *testing.T) {
-	testCases := map[string]struct {
-		input    interface{}
-		expected string
-	}{
-		"Given I input a string": {
-			input:    "123",
-			expected: "123",
-		},
-		"Given I input a bool with the value false": {
-			input:    false,
-			expected: "false",
-		},
-		"Given I input a bool with the value true": {
-			input:    true,
-			expected: "true",
-		},
-		"Given I input an int64": {
-			input:    int64(123),
-			expected: "123",
-		},
-		"Given I input an int": {
-			input:    123,
-			expected: "123",
-		},
-		"Given I input a float64": {
-			input:    float64(2.5),
-			expected: "2.5",
-		},
-		"Given I input a float32": {
-			input:    float32(2.5),
-			expected: "2.5",
-		},
-		"Given I input a nil value": {
-			input:    nil,
-			expected: "nil",
-		},
-	}
-
-	for desc, tc := range testCases {
-		tc := tc
-		t.Run(desc, func(t *testing.T) {
-
-			actual := convertInterfaceToString(tc.input)
-			if actual != tc.expected {
-				t.Errorf("(%s): expected %s, actual %s", desc, tc.expected, actual)
-			}
-		})
-	}
-}
-
 func TestListenerHandlesEventsCorrectly(t *testing.T) {
 	noOpLogger := logger.NewNoOpLogger() // Assume a constructor exists for the noOpLogger
 
@@ -155,6 +104,57 @@ func TestListenerHandlesEventsCorrectly(t *testing.T) {
 				}
 			}
 			service.targetAnalyticsMx.Unlock()
+		})
+	}
+}
+
+func Test_convertInterfaceToString(t *testing.T) {
+	testCases := map[string]struct {
+		input    interface{}
+		expected string
+	}{
+		"Given I input a string": {
+			input:    "123",
+			expected: "123",
+		},
+		"Given I input a bool with the value false": {
+			input:    false,
+			expected: "false",
+		},
+		"Given I input a bool with the value true": {
+			input:    true,
+			expected: "true",
+		},
+		"Given I input an int64": {
+			input:    int64(123),
+			expected: "123",
+		},
+		"Given I input an int": {
+			input:    123,
+			expected: "123",
+		},
+		"Given I input a float64": {
+			input:    float64(2.5),
+			expected: "2.5",
+		},
+		"Given I input a float32": {
+			input:    float32(2.5),
+			expected: "2.5",
+		},
+		"Given I input a nil value": {
+			input:    nil,
+			expected: "nil",
+		},
+	}
+
+	for desc, tc := range testCases {
+		tc := tc
+		t.Run(desc, func(t *testing.T) {
+
+			actual := convertInterfaceToString(tc.input)
+			if actual != tc.expected {
+				t.Errorf("(%s): expected %s, actual %s", desc, tc.expected, actual)
+			}
 		})
 	}
 }
