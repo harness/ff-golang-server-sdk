089f0c542d40726a1b62a9a0231b2a85ee13f05c
Erdi Rowlands <erdirowlands@gmail.com>
2024-04-16 19:18:08 +0100

    FFM-11212 New test for listener

--- analyticsservice/analytics_test.go
+++ analyticsservice/analytics_test.go
@@ -1,6 +1,13 @@
 package analyticsservice
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/harness/ff-golang-server-sdk/evaluation"
+	"github.com/harness/ff-golang-server-sdk/logger"
+	"github.com/harness/ff-golang-server-sdk/rest"
+)
 
 func Test_convertInterfaceToString(t *testing.T) {
 	testCases := map[string]struct {
@@ -51,4 +58,39 @@ func Test_convertInterfaceToString(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestListenerHandlesEventsCorrectly(t *testing.T) {
+	noOpLogger := logger.NewNoOpLogger() // assuming you have a constructor for a noOpLogger
+	service := NewAnalyticsService(1*time.Minute, noOpLogger)
+	defer close(service.analyticsChan) // Ensure the channel is closed after test
+
+	target := &evaluation.Target{Identifier: "target1", Anonymous: new(bool)}
+	featureConfig := &rest.FeatureConfig{Feature: "feature1"}
+	variation := &rest.Variation{Identifier: "var1", Value: "value1"}
+
+	// Send an event to the channel
+	go func() {
+		service.analyticsChan <- analyticsEvent{
+			target:        target,
+			featureConfig: featureConfig,
+			variation:     variation,
+		}
+	}()
+
+	// Allow some time for the event to be processed
+	time.Sleep(100 * time.Millisecond)
+
+	// Check if the event is processed correctly
+	service.evaluationsAnalyticsMx.Lock()
+	if len(service.evaluationAnalytics) != 1 {
+		t.Errorf("Expected evaluationAnalytics to contain 1 item, got %d", len(service.evaluationAnalytics))
+	}
+	service.evaluationsAnalyticsMx.Unlock()
+
+	service.seenTargetsMx.RLock()
+	if !service.seenTargets["target1"] {
+		t.Errorf("Expected target 'target1' to be marked as seen")
+	}
+	service.seenTargetsMx.RUnlock()
 }
