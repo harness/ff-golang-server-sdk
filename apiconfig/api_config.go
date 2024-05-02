4a9797237528c49edd4d5285050c745690a0cda4
Erdi Rowlands <erdirowlands@gmail.com>
2024-05-02 11:11:11 +0100

    FFM-11360 Comment

--- apiconfig/api_config.go
+++ apiconfig/api_config.go
@@ -2,6 +2,7 @@ package apiconfig
 
 import "github.com/harness/ff-golang-server-sdk/rest"
 
+// ApiConfiguration is a type that provides the required configuration for requests
 type ApiConfiguration interface {
 	GetSegmentRulesV2QueryParam() *rest.SegmentRulesV2QueryParam
 }
